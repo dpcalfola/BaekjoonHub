64c7fde728ec5edddf6e1e425e08c6e29ec6f103
Fola Flor <89129145+dpcalfola@users.noreply.github.com>
2022-04-04 09:18:55 +0900

    [Silver IV] Title: 큐, Time: 44 ms, Memory: 79512 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+// Q10845
+
+var queue: [Int] = []
+let N = Int(readLine()!)!
+
+for _ in 0..<N {
+	let input:[String] = readLine()!.components(separatedBy: " ").map { String($0) }
+	let firstIn = input[0]
+	switch firstIn {
+		case "push":
+			queue.append(Int(input[1])!)
+	 	case "pop":
+			queue.isEmpty ? print(-1) : print(queue.removeFirst())
+		case "size":
+			print(queue.count)
+		case "empty":
+			queue.isEmpty ? print(1) : print(0)
+		case "front":
+			queue.isEmpty ? print(-1) : print(queue.first!)
+		case "back":
+			queue.isEmpty ? print(-1) : print(queue.last!)
+		default:
+			break
+	}
+}
