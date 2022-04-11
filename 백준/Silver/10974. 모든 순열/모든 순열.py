619e7894b5d17258438d45657580d2298a58d700
Fola Flor <89129145+dpcalfola@users.noreply.github.com>
2022-04-11 21:07:05 +0900

    [Silver III] Title: 모든 순열, Time: 240 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.py
@@ -0,0 +1,19 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+arr = []
+
+
+def dfs():
+    if len(arr) == N:
+        print(*arr)
+        return
+    for i in range(1, N + 1):
+        if i not in arr:
+            arr.append(i)
+            dfs()
+            arr.pop()
+
+
+dfs()
