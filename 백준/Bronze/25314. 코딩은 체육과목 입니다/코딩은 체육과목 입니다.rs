04d4d10d9398e4de2dd1456895618e7aff031a54
Fola Flor <89129145+dpcalfola@users.noreply.github.com>
2023-03-05 21:42:59 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 4 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.rs
@@ -0,0 +1,28 @@
+use std::io;
+
+fn main() {
+    // read line
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Fail to read line");
+
+    // Convert string to integer
+    let num_of_input: i16 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Fail to convert to integer");
+            return;
+        }
+    };
+
+    let num_of_long: i16 = num_of_input / 4;
+    
+    // Declare and initialize
+    let mut answer: String = Default::default();
+    
+    for _i in 0..num_of_long {
+        answer += "long ";
+    }
+    answer += "int";
+
+    println!("{}", answer);
+}
