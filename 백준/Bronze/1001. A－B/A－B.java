0e7805d59e7db56348c954ee0548f4bb688c9bb1
Fola Flor <89129145+dpcalfola@users.noreply.github.com>
2024-03-23 21:28:20 +0900

    [Bronze V] Title: A-B, Time: 204 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a, b;
+        a = scan.nextInt();
+        b = scan.nextInt();
+        System.out.println(a-b);
+
+    }
+}
